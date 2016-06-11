cea606b45f3ddb2970c2e98e8e013c4815a40c8f
Fahad Kamran <fhdkmrn@gmail.com>
2016-06-10 23:16:04 -0700

    Updated

---
+++ MessingAround/resources/cs8/js/sols.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.solution-toggle-p').click(function() {
+	    $('.sol').toggle();
+	    $('.q').toggle();
+	});
+});
+
+$(document).ready(function(){
+	$('.solution-toggle-p2').click(function() {
+	    $('.sol2').toggle();
+	    $('.q2').toggle();
+	});
+});
+
+$(document).ready(function(){
+	$('.solution-toggle-p3').click(function() {
+	    $('.sol3').toggle();
+	    $('.q3').toggle();
+	});
+});
+
