3cb0117bf3addd2d7e56d5786ae4e5c9d8f62f4c
Fahad Kamran <fhdkmrn@gmail.com>
2016-06-10 23:17:19 -0700

    Updated

---
+++ Personal/resources/cs70/js/sols.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.solution-toggle-p').click(function() {
+	    $('.sol').toggle();
+	});
+});
+
+$(document).ready(function(){
+	$('.solution-toggle-p2').click(function() {
+	    $('.sol2').toggle();
+	});
+});
+
+$(document).ready(function(){
+	$('.solution-toggle-p3').click(function() {
+	    $('.sol3').toggle();
+	});
+});
+
